5515f921076152b4bace01e3d7de474098ae45bf
Sergii Kononenko <konan88@inbox.ru>
2023-08-23 14:30:24 +0300

    Initial Project add

---
+++ src/test/java/com/itkon/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itkon.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
