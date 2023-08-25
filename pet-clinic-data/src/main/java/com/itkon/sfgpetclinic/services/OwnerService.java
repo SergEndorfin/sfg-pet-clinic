d59f63bfc1db0b0e070e8fc53593d59f843ad798
Sergii Kononenko <konan88@inbox.ru>
2023-08-25 09:40:26 +0300

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/itkon/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package com.itkon.sfgpetclinic.services;
+
+import com.itkon.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
